b6099c81cee6fbcd382c130cc27d13f4fc0dc51d
Ricardo Rubio <rickarubio@gmail.com>
2015-02-06 21:46:37 -0800

    Creates Comment component and adds it to CommentList

--- public/scripts/components.js
+++ public/scripts/components.js
@@ -21,7 +21,35 @@ var CommentList = React.createClass({
       React.createElement(
         'div',
         { className: 'commentList' },
-        'Hola Mundo yo soy un comment list!'
+        'Hola Mundo yo soy un comment list!',
+        React.createElement(
+          Comment,
+          { author: 'Pete Hunt' },
+          'This is one comment'
+        ),
+        React.createElement(
+          Comment,
+          { author: 'Jordan Walke' },
+          'This is another comment'
+        )
+      )
+    );
+  }
+});
+
+var Comment = React.createClass({
+  displayName: 'Comment',
+  render: function() {
+    return (
+      React.createElement(
+        'div',
+        { className: 'comment' },
+        React.createElement(
+          'h2',
+          { className: 'commentAuthor' },
+          this.props.author
+        ),
+        this.props.children
       )
     );
   }
